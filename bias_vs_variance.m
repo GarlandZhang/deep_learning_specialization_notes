c8d916f3892f41060cbc069cada72dc27e826df3
itsGarland <garland._zhang@hotmail.com>
2020-08-04 16:03:36 -0400

    Create bias_vs_variance.m

---
+++ bias_vs_variance.m
@@ -0,0 +1,11 @@
+## bias (underfit)
+- high training cost error
+- cross validation cost will be approximately = train cost
+
+
+## variance (overfit)
+- low train cost
+- cv cost >> train cost
+
+
+![bv](https://i.gyazo.com/d1a42514aa8fbc5fa421922e2a07f064.png)
